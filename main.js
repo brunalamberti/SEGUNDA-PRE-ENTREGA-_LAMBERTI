7e2178dd3b81f66a669638616711ada05fbe5c2a
brunalamberti <brunalamberti96@gmail.com>
2023-04-02 17:36:12 -0300

    Segunda pre-entrega

---
+++ main.js
@@ -0,0 +1,133 @@
+const carrito = [];
+
+alert("Bienvenidos a mi tienda online");
+
+const inicioNav = () =>{
+    const ordenProds = confirm('¿Querés ordenar los productos del más barato al mas caro?')
+
+    if (ordenProds) {
+        ordenarPrecioAsc()
+    } else {
+        ordenarPrecioAleat()
+    }
+}
+
+
+const ordenarPrecioAsc = () =>{
+    productos.sort((a,b)=> a.precio - b.precio);
+    mostrarListaOrdenada();
+}
+
+const ordenarPrecioAleat = () => {
+    productos.sort((a,b) => 0.5 - Math.random());
+    mostrarListaOrdenada();
+}
+
+const mostrarListaOrdenada = ()=>{
+    const listaOrdenada = productos.map(producto => {
+        return '#' + producto.id + '-' + producto.nombre + ' $'+ producto.precio;
+    });
+    alert('Lista de precios:'+'\n\n'+listaOrdenada.join('\n'))
+    comprarProductos(listaOrdenada)
+
+}
+
+const comprarProductos = (listaDeProductos) =>{
+    let prodNombre = '';
+    let otroProducto = false;
+    let prodCant = 0;
+
+    do {
+        prodNombre = prompt ('¿Que producto desea comprar?'+'\n\n'+listaDeProductos.join('\n'));
+        prodCant = parseInt(prompt('¿Cuántos querés comprar?'));
+
+        const prodPorNombre = productos.find(producto => producto.nombre.toLowerCase() === prodNombre.toLowerCase());
+        const prodPorId = productos.find(producto => producto.id === prodNombre);
+        const prodIndex = productos.indexOf(prodNombre.toLowerCase());
+        
+        if (prodPorNombre || prodPorId) {
+            verificarStock(prodIndex, prodCant)
+        } else {
+            alert('El producto no se encuentra en el catálogo.')
+        }
+
+        otroProducto = confirm('¿Desea agregar otro producto?');
+    } while (otroProducto)
+
+    confirmarCompra()
+
+}
+
+const verificarStock = (index, cantidad)=>{
+    cantidad<=productos[index].cant? agregarAlCarrito(productos[index],productos[index].id, cantidad) :
+    alert(`Solo quedan ${productos[index].cant} unidades del producto ${productos[index].nombre}`)
+}
+
+const agregarAlCarrito = (producto, productoId, cantidad) =>{
+    const productoRepetido = carrito.find(producto => producto.id === productoId)
+    if (!productoRepetido) {
+        producto.cant += cantidad
+        carrito.push(producto)
+    } else {
+        productoRepetido.cant += cantidad
+    }
+}
+
+const confirmarCompra = () => {
+    const listaCarrito = carrito.map(producto => {
+        return '- '+producto.nombre+' | Cantidad: '+producto.cant
+    });
+
+    const confirmar = confirm('Checkout: '
+        +'\n\n'+listaCarrito.join('\n')
+        +'\n\nPara continuar presione "Aceptar". Presione "Cancelar" para eliminar un producto.'
+    )
+
+    if (confirmar) {
+        finalizarCompra(listaCarrito)
+    } else {
+        const productoAEliminar = prompt('Ingrese el nombre del producto a eliminar:')
+        eliminarProductoCarrito(productoAEliminar)
+    }
+};
+const eliminarProductoCarrito = (productoNombre) => {
+    carrito.forEach((producto, index) => {
+        if (producto.nombre.toLowerCase() === productoNombre.toLowerCase()) {
+            if (producto.cantidad > 1) {
+                producto.cantidad--
+            } else {
+                carrito.splice(index, 1)
+            }
+        }
+    })
+    confirmarCompra()
+};
+
+const finalizarCompra = (listaCarrito) => {
+    const cantidadTotal = carrito.reduce((acc, item) => acc + item.cantidad, 0)
+    const precioTotal = carrito.reduce((acc, item) => acc + (item.precio * item.cantidad), 0)
+
+    alert('Detalle de su compra:'
+        +'\n\n'+listaCarrito.join('\n')
+        +'\n\nTotal de productos: '+cantidadTotal
+        +'\n\nEl total de su compra es: '+precioTotal
+        +'\n\nGracias por su compra!!!'
+    )
+};
+
+inicioNav();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ stock.js
@@ -0,0 +1,68 @@
+const productos = [
+{
+    id: 1,
+    nombre: "Fideos",
+    tipo: "fideos",
+    descripción: "fideos secos",
+    precio: 200,
+    pesoEnGrs: 500,
+    cant: 10
+
+
+},
+{
+    id: 2,
+    nombre: "Salsa",
+    tipo: "salsa",
+    descripción: "salsa de tomate lista",
+    precio: 250,
+    pesoEnGrs: 300,
+    cant: 10
+
+
+},
+{
+    id: 3,
+    nombre: "Queso rallado",
+    tipo: "queso",
+    descripción: "queso rallado",
+    precio: 600,
+    pesoEnGrs: 150,
+    cant: 10
+
+
+},
+{
+    id: 4,
+    nombre: "Pan",
+    tipo: "pan",
+    descripción: "baguette",
+    precio: 120,
+    pesoEnGrs: 250,
+    cant: 10
+
+
+},
+{
+    id: 5,
+    nombre: "Aceite de oliva",
+    tipo: "aceite",
+    descripción: "Aceite de oliva",
+    precio: 900,
+    pesoEnGrs: 680,
+    cant: 10
+
+
+},
+{
+    id: 6,
+    nombre: "Chocolate",
+    tipo: "chocolate",
+    descripción: "chocolate amargo",
+    precio: 400,
+    pesoEnGrs: 120,
+    cant: 10
+
+
+},
+];
